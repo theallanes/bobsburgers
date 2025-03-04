07b7caa975dde052cc9ec1cee015ea29518d0805
Thea <zllanes@academic.rrc.ca>
2025-03-04 13:11:38 -0600

    added validations to character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,7 @@
 class Character < ApplicationRecord
   belongs_to :actor
   has_many :episodes, through: :character_episodes
+
+  validates :name, :gender, presence: true
+  validates :age, numericality: { only_integer: true }
 end
