ff233a83f6d6052bef6b122a8cb4906567d51af7
Thea <zllanes@academic.rrc.ca>
2025-03-04 10:24:04 -0600

    Tested seed script by fetching API

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'httparty'
+
+url = 'https://bobsburgers-api.herokuapp.com/'
+response = HTTParty.get(url)
+
+response.each do |release|
+  puts release["description"]
+end
