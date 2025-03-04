7fa41e5c98031e33c03f5a50e74c96bfcdc65759
Thea <zllanes@academic.rrc.ca>
2025-03-04 15:12:08 -0600

    added validations to episode model

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,8 @@
 class Episode < ApplicationRecord
   has_many :stores, :burgers
   has_many :characters, through: :character_episodes
+
+  validates :title, :description, :air_date, :season, :episode_number, presence: true
+  validates :season, :episode_number, numericality: { only_integer: true }
+  validates :title, uniqueness: true
 end
