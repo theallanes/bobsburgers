563eec6963f8167c6121148a53965717a900c425
Thea <zllanes@academic.rrc.ca>
2025-03-05 17:26:59 -0600

    Edited characters controller and views file

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,9 @@
 class CharactersController < ApplicationController
   def index
+    @characters = Character.order("name DESC")
   end
 
   def show
+    @character = Character.find(params[:id])
   end
 end
