ea6f055f5f2d8f07d77fd35317d0a8e019d534ce
Thea <zllanes@academic.rrc.ca>
2025-03-05 17:35:27 -0600

    CReated episodes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "episodes/index"
+  get "episodes/show"
   resources :characters, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
