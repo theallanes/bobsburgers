c2eaea695b240486bb5de1589b9e3e874f2fd0c5
Thea <zllanes@academic.rrc.ca>
2025-03-05 13:53:38 -0600

    Imported csv file for voice actors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,47 @@
 require 'httparty'
 
-# CharacterEpisode.destroy_all
-# Character.destroy_all
-# Burger.destroy_all
-# Store.destroy_all
-# Actor.destroy_all
+CharacterEpisode.destroy_all
+Character.destroy_all
+Burger.destroy_all
+Store.destroy_all
+Actor.destroy_all
 # Episode.destroy_all
 
 ep_url = 'https://bobsburgers-api.herokuapp.com/episodes'
 character_url = 'https://bobsburgers-api.herokuapp.com/characters'
+burger_url = 'https://bobsburgers-api.herokuapp.com/burgerOfTheDay'
+store_url = 'https://bobsburgers-api.herokuapp.com/storeNextDoor'
 episodes = HTTParty.get(ep_url)
 characters = HTTParty.get(character_url)
+burgers = HTTParty.get(burger_url)
+stores = HTTParty.get(store_url)
 
 
-episodes.each do |episode|
-  Episode.create(
-    title: episode["name"],
-    description: episode["description"],
-    air_date: episode["airDate"],
-    season: episode["season"],
-    episode_number: episode["episode"]
-  )
-end
+# episodes.each do |episode|
+#   Episode.create(
+#     title: episode["name"],
+#     description: episode["description"],
+#     air_date: episode["airDate"],
+#     season: episode["season"],
+#     episode_number: episode["episode"]
+#   )
+# end
 
 characters.each do |character|
-  ep = Episode.find_by(title: character["firstEpisode"])
+  first_episode = character["firstEpisode"]
+
+  if first_episode.nil?
+    puts "No episode title found for character #{character["name"]}. Skipping..."
+    next
+  end
+
+  first_episode = first_episode.strip.downcase
+  ep = Episode.find_by("LOWER(title) = ?", first_episode)
+
+  if ep.nil?
+    puts "Episode not found for character #{character["name"]}"
+    next
+  end
 
   char = Character.find_or_create_by(
     name: character["name"],
@@ -35,5 +52,9 @@ characters.each do |character|
 
   if char.persisted?
     CharacterEpisode.create(character: char, episode: ep)
+    puts "Character #{char.name} associated with episode #{ep.title}"
+  else
+    puts "Failed to create #{character["name"]}"
   end
 end
+
