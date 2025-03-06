1c73e4a686a8e0254c852b8dc00863fc589bcae6
Thea <zllanes@academic.rrc.ca>
2025-03-06 12:57:59 -0600

    updated models

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,4 +4,5 @@ class Character < ApplicationRecord
   has_many :episodes, through: :character_episodes
 
   validates :name, :gender, presence: true
+  validates :name, length: {maximum: 100}
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,5 @@
 class Store < ApplicationRecord
   belongs_to :episode
 
-  validates :store_name, presence: true
+  validates :store_name, presence: true, uniqueness: true
 end
