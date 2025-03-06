14f4a1ed76b4a85a52eac480d4e815735d1aab3e
Thea <zllanes@academic.rrc.ca>
2025-03-05 20:25:59 -0600

    Edited burger controller

--- app/controllers/burgers_controller.rb
+++ app/controllers/burgers_controller.rb
@@ -1,7 +1,9 @@
 class BurgersController < ApplicationController
   def index
+    @burgers = Burger.order("id ASC").limit(100)
   end
 
   def show
+    @burger = Burger.find(params[:id])
   end
 end
