35a3e93ede45dcbc05e80b0e9ea33c21aa23956c
Thea <zllanes@academic.rrc.ca>
2025-03-05 23:03:53 -0600

    Edited actor controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,7 +1,9 @@
 class ActorsController < ApplicationController
   def index
+    @actors = Actor.order("actor_name ASC").limit(100)
   end
 
   def show
+    @actor = Actor.find(params[:id])
   end
 end
